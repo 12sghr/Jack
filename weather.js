e118be06a4386258314206b12dc25ea60f8d9606
杉原 <shinta_16n1200325@nnn.ed.jp>
2017-01-28 11:39:40 +0900

    Use cron

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 const MySlackBot = require("./slackBot");
+const CronJob = require("cron").CronJob;
 const request = require("request-promise");
 
 const mySlackBot = new MySlackBot();
@@ -13,7 +14,7 @@ class Weather{
     }
 
     pushWeather(){
-        if(this.currentTime.getHours() === 6){
+        const job = new CronJob("30 6 * * *", () => {
             this.options.uri += ("1850144&units=metric&cnt=1&APPID=" + process.env.APIID_OPENWEATHERMAP);
             request.get(this.options)
             .then((res) => {
@@ -25,7 +26,8 @@ class Weather{
                 let percentClouds = res.list[0].clouds;
                 mySlackBot.replyChannel("test", "今日の東京\n天気 " + weather + "\n雲量 " + percentClouds + "\n最高気温 " + maxTemp + "℃\n最低気温 " + minTemp + "℃\n湿度 " + humidity + "％\n風速 " + windSpeed + "m/s");
             });
-        }
+        }, null, false, "Asia/Tokyo");
+        job.start();
     }
 
 
